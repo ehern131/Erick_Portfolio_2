5ab4af056734ac39c7852d5d3791418acce5816e
Erick Hernandez <ehern131@fiu.edu>
2017-03-15 01:24:32 -0400

    Work Dropdown and Travelize update

--- public/js/effects.js
+++ public/js/effects.js
@@ -6,3 +6,26 @@
 //     $(this).animate({height: "-=20" });
 //   })
 // });
+
+
+$(document).ready(function(){
+  var scroll_pos = 0;
+  $(document).scroll(function() {
+    scroll_pos = $(this).scrollTop();
+    if(scroll_pos > 30) {
+      $("#workList").css({
+        'background-color' : 'white',
+        'transition-duration': '.3s',
+        'transition-property': 'all',
+        'transition-timing-function': 'cubic-bezier(.7,1,.7,1)',
+      });
+      $("#workList a").css('color', '#515769');
+    }
+    else {
+      $("#workList").css({
+        'background-color': 'rgba(0,0,0,0)',
+      });
+      $("#workList a").css('color', 'white');
+    }
+  });
+});
