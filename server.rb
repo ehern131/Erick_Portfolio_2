b2c60ba4a719efd75a4a4a8038cdc4b25ff56bd8
Erick Hernandez <ehern131@fiu.edu>
2017-11-28 22:58:05 -0500

    Add Route to Index2

--- server.rb
+++ server.rb
@@ -3,3 +3,9 @@ require 'sinatra'
 get '/' do
   File.read(File.join('public','index.html'))
 end
+get '/index' do
+  File.read(File.join('public','index.html'))
+end
+get '/index2' do
+  File.read(File.join('public','index2.html'))
+end
